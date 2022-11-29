a2ad7335bbd30d8eda4b16b2210977882a8fcb8b
ct kt <ct4g00gle@gmail.com>
2022-11-29 23:36:07 +0530

    if part also committed

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry?")
 if hungry=="yes" or hungry=="Yes":
     print("eat samosa")
+    print("eat dhokla")
 else:
     print("do your homework")
     print("yes all changes happened....")
