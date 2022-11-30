57780d1df5fcc0c6d12fec55b373d5aee0b1f440
ct kt <ct4g00gle@gmail.com>
2022-11-30 06:56:53 +0530

    water

--- hungry.py
+++ hungry.py
@@ -4,5 +4,8 @@ if hungry=="yes" or hungry=="Yes":
     print("eat dhokla")
 else:
     print("do your homework")
+    thirsty = input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
     print("yes all changes happened....")
 print("Nice work to done...")
